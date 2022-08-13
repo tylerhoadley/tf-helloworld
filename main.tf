148196ccf1ac319cf5e25185d81adeaab8940ce0
Noah Burrell <noah@frontenac.tech>
2022-08-13 14:59:54 -0400

    Update main.tf
    update

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.1.0"
+  cloud {
+    organization = "Frontenac-Technoloogy-Services"
+
+    workspaces {
+      name = "helloworld"
+    }
+  }
 }
 
 variable "subject" {
