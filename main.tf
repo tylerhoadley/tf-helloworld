ee611a4fed34ba0c97b02ff2931a26b97dfa9a95
Noah Burrell <noah@frontenac.tech>
2022-08-14 09:26:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,26 @@ variable "subject" {
    description = "Subject to hello"
 }
 
+
+variable "test1" {
+   type = string
+}
+variable "test2" {
+   type = string
+}
+variable "secret1" {
+   type = string
+   sensitive = true
+}
+variable "secret2" {
+   type = string
+   sensitive = true
+}
+
 output "hello_world" {
   value = "Hello, ${var.subject}!"
+}
+
+output "cm_and_secret" {
+  value = "test1: ${var.test1} | test2: ${var.test2} | secret1: ${var.secret1} | secret2: ${var.secret2}" 
 }
